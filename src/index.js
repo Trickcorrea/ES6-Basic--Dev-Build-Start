b9ade96adee0f59bbe66d35d75c0e041aab699d2
Patrick correa <kakike15@yahoo.com.br>
2019-06-29 19:06:18 -0300

    exemplo de map() codigo ES6

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // code ES6 HER
 let arr = [1,2,3,4,5,6]
-const variavel = arr.map((value) => value + 1 )
-console.log(variavel)
+const resultado = arr.map((value) => value + 1 )
+console.log(resultado)
